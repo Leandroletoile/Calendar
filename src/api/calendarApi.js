5c9c4a17eaa6a1d87ba89e45ed20934596f0b994
Leandro Letoile <leandroletoile@hotmail.com>
2022-12-05 23:08:36 -0300

    last

--- src/api/calendarApi.js
+++ src/api/calendarApi.js
@@ -1,10 +1,7 @@
 import axios from 'axios';
-import { getEnvVariables } from '../helpers/getEnvVariables';
-
-const { VITE_API_URL } = getEnvVariables();
 
 const calendarApi = axios.create({
-    baseURL: VITE_API_URL,
+    baseURL: process.env.VITE_API_URL,
 })
 
 
